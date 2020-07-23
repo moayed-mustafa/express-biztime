0e361265ebdeb1cd0d9b86a8a132ce61ef3e6237
moayed-mustafa <moayed.abrida@yahoo.com>
2020-07-23 23:59:33 +0400

    added some tests ... slowly

--- test_database.sql
+++ test_database.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS biztime;
-CREATE DATABASE biztime;
+DROP DATABASE IF EXISTS biztime_test;
+CREATE DATABASE biztime_test;
 
-\c biztime;
+\c biztime_test;
 
 DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS companies;
@@ -22,12 +22,3 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
-INSERT INTO companies
-  VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
-         ('ibm', 'IBM', 'Big blue.');
-
-INSERT INTO invoices (comp_Code, amt, paid, paid_date)
-  VALUES ('apple', 100, false, null),
-         ('apple', 200, false, null),
-         ('apple', 300, true, '2018-01-01'),
-         ('ibm', 400, false, null);
---
+++ tests/companies.test.js
@@ -0,0 +1,74 @@
+const request = require('supertest')
+const app = require('../app')
+// set the environemt variable before requiring db
+process.env.NODE_ENV === 'test'
+const db = require('../db')
+
+/**
+ * What I want to test:
+ * getting all companies
+ * /companies
+ * getting company by code
+ * /companies/code
+ * posting a company
+ * /companies/create-company
+ * editing a company by code
+ * /companies/code
+ * deliting a company by code
+ * /companies/code
+ */
+
+
+//  Test
+/**
+ * before each test, create a company
+ * delete the company after each test
+ * close the db connection after all the tests has run
+ */
+let testCompany
+let testInvoice
+beforeEach(async () => {
+    const comp= await db.query(`
+    INSERT INTO companies (code, name, description)
+    VALUES ('MOE_SP', 'Moayed sport equipments', 'making amazing things for all sports')
+
+    RETURNING *`)
+
+    const invoice= await db.query(`
+    INSERT INTO invoices  (comp_Code, amt, paid, paid_date)
+    VALUES('MOE_SP', 300, true, '2018-01-01')
+
+    RETURNING *`)
+    testCompany = comp.rows[0]
+    testInvoice = invoice.rows[0]
+})
+
+afterEach(async () => {
+    await db.query('DELETE FROM companies')
+})
+
+afterAll(async () => {
+    await db.end()
+})
+
+
+describe('GET/companies', () => {
+    test('test reading companies', async () => {
+
+        const res = await request(app).get('/companies')
+        expect(res.statusCode).toEqual(200)
+
+        expect(res.body).toEqual({companies:[testCompany]})
+
+    })
+})
+describe('GET/companies/code', () => {
+    test('test reading a company', async () => {
+
+        const res = await request(app).get(`/companies/${testCompany.code}`)
+        expect(res.statusCode).toEqual(200)
+        expect(res.body.results).toEqual(testCompany)
+
+
+    })
+})
